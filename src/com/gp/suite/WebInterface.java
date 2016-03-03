90f6c2b9df9396d65eca6c56efdaa071e708afc7
Martimor <Martin.Schmidt@1mpress.de>
2016-03-03 10:04:44 +0100

    Neu

--- src/com/gp/suite/WebInterface.java
+++ src/com/gp/suite/WebInterface.java
@@ -1,4 +1,5 @@
 package com.gp.suite;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WebInterface extends JavaPlugin {
@@ -7,10 +8,11 @@ public class WebInterface extends JavaPlugin {
     @Override
     public void onEnable() {
     	this.getCommand("test").setExecutor(new CommandTest());
+    	getServer().getPluginManager().registerEvents(new MyListener(), this);
     }
+    
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
-
     }
 }
