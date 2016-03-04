99d60c280e1d321d9df37e2ea43d5e5d93a85d0a
Martimor <Martin.Schmidt@1mpress.de>
2016-03-04 19:37:34 +0100

    divers

--- src/com/gp/suite/ActionListener.java
+++ src/com/gp/suite/ActionListener.java
@@ -3,16 +3,12 @@ package com.gp.suite;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Map;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,8 +17,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldguard.bukkit.RegionContainer;
 import com.sk89q.worldguard.bukkit.WGBukkit;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.managers.storage.StorageException;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
@@ -32,19 +31,22 @@ public class ActionListener implements Listener {
     public void onPlayerUse(PlayerInteractEvent event){
         Player p = event.getPlayer();
         
-        if(event.getClickedBlock().getType() == Material.SIGN_POST)
-        {    
-            Sign s = (Sign) event.getClickedBlock().getState();
-            
-            if(s.getLine(0).equalsIgnoreCase("GP Real Estate")){
-            	buyEstate(event.getClickedBlock().getLocation(), event.getPlayer());
-            }
-        } else
-        if(event.getClickedBlock().getType() == Material.DIRT && p.getItemInHand().getType() == Material.STICK)
-        {    
-        	if(!checkIsEstate(event.getClickedBlock().getLocation())) 
-        	if(setNewEstate(event.getClickedBlock())) 
-                p.sendMessage("Grundstück angelegt");
+        if(event.getClickedBlock() != null)
+        {
+	        if(event.getClickedBlock().getType() == Material.SIGN_POST)
+	        {    
+	            Sign s = (Sign) event.getClickedBlock().getState();
+	            
+	            if(s.getLine(0).equalsIgnoreCase("GP Real Estate")){
+	            	buyEstate(event.getClickedBlock().getLocation(), event.getPlayer());
+	            }
+	        } else
+	        if(event.getClickedBlock().getType() == Material.GRASS && p.getItemInHand().getType() == Material.STICK && p.isOp() == true)
+	        {    
+	        	if(!checkIsEstate(event.getClickedBlock().getLocation())) 
+	        	if(setNewEstate(event.getClickedBlock())) 
+	                p.sendMessage("Grundstück angelegt");
+	        }
         }
     }
 
@@ -72,50 +74,73 @@ public class ActionListener implements Listener {
 			
 			World world = block.getWorld();
 			
-			int x1 = 0;
-			int x2 = 0;
-			int z1 = 0;
-			int z2 = 0;
+			int x1 = 1;
+			int x2 = 1;
+			int z1 = 1;
+			int z2 = 1;
 			
-			while(new Location(world, block.getX() - x1, block.getY(), block.getZ()).getBlock().getType() == Material.DIRT)
+			while(new Location(world, block.getX() - x1, block.getY(), block.getZ()).getBlock().getType() == Material.GRASS)
 			{
 				x1++;
 				if(x1 > 500) { x1 = -1; break; }
 			}
 			
-			while(new Location(world, block.getX() + x2, block.getY(), block.getZ()).getBlock().getType() == Material.DIRT)
+			while(new Location(world, block.getX() + x2, block.getY(), block.getZ()).getBlock().getType() == Material.GRASS)
 			{
 				x2++;
 				if(x2 > 500) { x2 = -1; break; }
 			}
 			
-			while(new Location(world, block.getX() - z1, block.getY(), block.getZ()).getBlock().getType() == Material.DIRT)
+			while(new Location(world, block.getX(), block.getY(), block.getZ() - z1).getBlock().getType() == Material.GRASS)
 			{
 				z1++;
 				if(z1 > 500) { z1 = -1; break; }
 			}
 			
-			while(new Location(world, block.getX() + z1, block.getY(), block.getZ()).getBlock().getType() == Material.DIRT)
+			while(new Location(world, block.getX(), block.getY(), block.getZ() + z2).getBlock().getType() == Material.GRASS)
 			{
 				z2++;
 				if(z2 > 500) { z2 = -1; break; }
 			}
-			if(x1 != -1 && x2 != -1 && z1 != -1 && z2 != -1)
-			{
-			new Location(world, x1, block.getY(), z1).getBlock().setType(Material.SIGN_POST);
-			Sign s = (Sign) new Location(world, x1, block.getY(), z1).getBlock().getState();
-			s.setLine(0, "GP Real Estate");
-			s.setLine(1, x1 + "x" + x2 + "|" + z1 + "x" + z2);
-			s.update();
 			
-	    	statement.executeUpdate("INSERT INTO gp_mc_estate (`x1`, `x2`, `z1`, `z2`) VALUES ('"+x1+"','"+x2+"','"+z1+"','"+z2+"')");
+			if(x1 != -1 && x2 != -1 && z1 != -1 && z2 != -1)
+			{		    	
+			x1 = block.getX() - x1 + 1;
+			x2 = block.getX() + x2 - 1;
+
+			z1 = block.getZ() - z1 + 1;
+			z2 = block.getZ() + z2 - 1;
+			
+	    	WorldGuardPlugin wg = WGBukkit.getPlugin();
+	    	
+	    	RegionContainer container = wg.getRegionContainer();
+	    	RegionManager regions = container.get(world);
+	    	int numregions = regions.getRegions().size();
+
+			new Location(world, x1, block.getY(), z1 - 1).getBlock().setType(Material.SIGN_POST);
+			Sign s = (Sign) new Location(world, x1, block.getY(), z1 - 1).getBlock().getState();
+			((org.bukkit.material.Sign)s.getData()).setFacingDirection(BlockFace.NORTH);
+			s.setLine(0, "GP Real Estate");
+			s.setLine(1, "GS" + numregions);
+			s.update();
+
+			
+	    	statement.executeUpdate("INSERT INTO gp_mc_estate (`name`, `x1`, `x2`, `z1`, `z2`) VALUES ('" + "GS" + numregions + "', '"+x1+"','"+x2+"','"+z1+"','"+z2+"')");
 	    	
 	    	BlockVector min = new BlockVector(x1, 0, z1);
 	    	BlockVector max = new BlockVector(x2, 255, z2);
-	    	ProtectedRegion region = new ProtectedCuboidRegion(x1 + "x" + x2 + "|" + z1 + "x" + z2, min, max);
-	    	Collection<ProtectedRegion> regions = new HashSet<ProtectedRegion>();
-	    	regions.add(region);
-	    	WGBukkit.getRegionManager(world).setRegions(regions);
+	    	ProtectedRegion region = new ProtectedCuboidRegion("GS" + numregions, min, max);    
+	    	
+
+	    	regions.addRegion(region);
+			
+	    	try {
+				regions.saveChanges();
+				regions.save();
+			} catch (StorageException e) {
+				e.printStackTrace();
+				return false;
+			}
 	    	
             return true;
 			}
